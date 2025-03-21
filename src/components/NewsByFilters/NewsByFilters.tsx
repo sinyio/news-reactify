0c60220dd1fb0094cf942b23af0d16dee6282291
Ilya Sinyakin <arqu1t@mail.ru>
2025-03-22 02:48:41 +0300

    add rtk, rtk query

--- src/components/NewsByFilters/NewsByFilters.tsx
+++ src/components/NewsByFilters/NewsByFilters.tsx
@@ -1,51 +1,48 @@
-import { getNews } from "../../api/apiNews";
-import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants";
+import { TOTAL_PAGES } from "../../constants/constants";
 import { useDebounce } from "../../helpers/hooks/useDebounce";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { useFilters } from "../../helpers/hooks/useFilters";
-import { NewsApiResponse, ParamsType } from "../../interfaces";
+import { useAppDispatch, useAppSelector } from "../../store";
+import { useGetNewsQuery } from "../../store/services/newsApi";
+import { setFilters } from "../../store/slices/newsSlice";
 import NewsFilters from "../NewsFilters/NewsFilters";
 import NewsList from "../NewsList/NewsList";
 import PaginationWrapper from "../PaginationWrapper/PaginationWrapper";
 import styles from "./styles.module.css";
 
 const NewsByFilters = () => {
-  const { filters, changeFilters } = useFilters({
-    page_number: 1,
-    page_size: PAGE_SIZE,
-    category: null,
-    keywords: "",
-  });
+  const dispatch = useAppDispatch();
+
+  const filters = useAppSelector((state) => state.news.filters);
 
   const debouncedKeywords = useDebounce(filters.keywords, 1500);
 
-  const { data, isLoading } = useFetch<NewsApiResponse, ParamsType>(
-    getNews,
-    {
-      ...filters,
-      keywords: debouncedKeywords,
-    }
-  );
+  const { data, isLoading } = useGetNewsQuery({
+    ...filters,
+    keywords: debouncedKeywords,
+  });
 
   const handleNextPage = () => {
     if (filters.page_number < TOTAL_PAGES) {
-      changeFilters("page_number", filters.page_number + 1);
+      dispatch(
+        setFilters({ key: "page_number", value: filters.page_number + 1 })
+      );
     }
   };
 
   const handlePreviousPage = () => {
     if (filters.page_number > 1) {
-      changeFilters("page_number", filters.page_number - 1);
+      dispatch(
+        setFilters({ key: "page_number", value: filters.page_number - 1 })
+      );
     }
   };
 
   const handlePageClick = (pageNumber: number) => {
-    changeFilters("page_number", pageNumber);
+    dispatch(setFilters({ key: "page_number", value: pageNumber }));
   };
 
   return (
     <section className={styles.section}>
-      <NewsFilters filters={filters} changeFilters={changeFilters} />
+      <NewsFilters filters={filters} />
 
       <PaginationWrapper
         top
--- src/components/NewsFilters/NewsFilters.tsx
+++ src/components/NewsFilters/NewsFilters.tsx
@@ -1,7 +1,8 @@
-import { getCategories } from "../../api/apiNews";
 import { useTheme } from "../../context/ThemeContext";
-import { useFetch } from "../../helpers/hooks/useFetch";
-import { CategoriesApiResponse, IFilters } from "../../interfaces";
+import { IFilters } from "../../interfaces";
+import { useAppDispatch } from "../../store";
+import { useGetCategoriesQuery } from "../../store/services/newsApi";
+import { setFilters } from "../../store/slices/newsSlice";
 import Categories from "../Categories/Categories";
 import Search from "../Search/Search";
 import Slider from "../Slider/Slider";
@@ -9,14 +10,12 @@ import styles from "./styles.module.css";
 
 interface Props {
   filters: IFilters;
-  changeFilters: (key: string, value: string | number | null) => void;
 }
 
-const NewsFilters = ({ filters, changeFilters }: Props) => {
+const NewsFilters = ({ filters }: Props) => {
   const { isDark } = useTheme();
-  const { data: dataCategories } = useFetch<CategoriesApiResponse, null>(
-    getCategories
-  );
+  const { data: dataCategories } = useGetCategoriesQuery(null);
+  const dispatch = useAppDispatch();
 
   return (
     <div className={styles.filters}>
@@ -25,7 +24,7 @@ const NewsFilters = ({ filters, changeFilters }: Props) => {
           <Categories
             categories={dataCategories.categories}
             setSelectedCategory={(category) =>
-              changeFilters("category", category)
+              dispatch(setFilters({ key: "category", value: category }))
             }
             selectedCategory={filters.category}
           />
@@ -34,7 +33,9 @@ const NewsFilters = ({ filters, changeFilters }: Props) => {
 
       <Search
         keywords={filters.keywords}
-        setKeywords={(keywords) => changeFilters("keywords", keywords)}
+        setKeywords={(keywords) =>
+          dispatch(setFilters({ key: "keywords", value: keywords }))
+        }
       />
     </div>
   );
